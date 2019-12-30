c818cf32b51d0fe1f6004ab87922c6a13ea5052e
rocky91star <jacobblessingskumarapu@gmail.com>
2019-12-30 18:51:08 +0530

    task completed

--- Backend/index.js
+++ Backend/index.js
@@ -7,38 +7,22 @@ let cors = require('cors');
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(cors());
+
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+   useNewUrlParser: true
+}).then(() => {
+   console.log('Successfully connected to the database');
+}).catch(err => {
+   console.log('Could not connect to the database. Exiting now...', err);
+   process.exit();
+});
+
+require('./app/routes/Employees.routes')(app);
+
 const port = 7000;
 app.listen(port, () => console.log("App is running on port 7000"));
-
-function read() {
-   return fs.readFileSync('./Task1.json', 'utf-8', (err, jsontring) => {
-      if (err) {
-         console.log('Error in fs', err);
-      } else {
-         return (jsontring);
-      }
-   })
-};
-
-app.get('/employees', (req, res) => {
-   let details = JSON.parse(read());
-   const employeesIds = details.employees.map(function ({ id, ...rest }) {
-      return { employeeId: id, ...rest }
-   });
-   res.json(employeesIds);
-});
-
-app.get('/employee', (req, res) => {
-   let filtr = JSON.parse(read());
-   let project1 = req.query.project;
-   const final = filtr.employees.filter(employees => employees.project === project1);
-   res.send(final);
-});
-
-app.get('/employee/ctc', (req, res) => {
-   let ctc = JSON.parse(read());
-   const total = ctc.employees.reduce(function (acc, { salary }) {
-      return acc + salary;
-   }, 0);
-   res.json(total);
-});
